80a0ac1ca516bf5492c2b2279513098b7f15bde9
sagarpithadiyasp <sagarpithadiya.sp@gmail.com>
2023-10-14 13:29:30 +0530

    old update

---
+++ animation.js
@@ -0,0 +1,14 @@
+<script src="three.r134.min.js"></script>
+<script src="vanta.birds.min.js"></script>
+<script>
+VANTA.BIRDS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
+</script>
